635c70f9c8d4004ddf82fbc551e091708f00a400
Isaac Iniongun <iniongunisaac@gmail.com>
2023-06-24 20:22:24 +0100

    create settings

--- TivBible/Features/Settings/SettingsScreen.swift
+++ TivBible/Features/Settings/SettingsScreen.swift
@@ -6,13 +6,52 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct SettingsScreen: View {
+    @EnvironmentObject private var preferenceStore: PreferenceStore
+    
     var body: some View {
-        Text("Settings")
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: 40) {
+                VStack {
+                    Image(.appLogo)
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .frame(width: 70, height: 70)
+                    
+                    Text("v3.0")
+                        .font(preferenceStore.font(size: 14))
+                        .foregroundStyle(.secondary)
+                }
+                
+                FontSizeView()
+                
+                LineSpacingView()
+                
+                FontStyleButton()
+                
+                AppThemesView()
+                
+                Toggle(isOn: $preferenceStore.stayAwake) {
+                    Text("STAY AWAKE:")
+                        .font(preferenceStore.font(size: 14))
+                        .foregroundStyle(.secondary)
+                }
+                .padding(.trailing, 2)
+            }
+        }
+        .padding()
+        .onChange(of: preferenceStore.stayAwake) { _, awake in
+            withAnimation {
+                UIApplication.shared.isIdleTimerDisabled = awake
+            }
+        }
     }
 }
 
 #Preview {
     SettingsScreen()
+        .environmentObject(PreferenceStore())
 }
